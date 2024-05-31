4226f9c0cf47d2ba7e7a02c1b8e21f804937a2ef
Alex Nordin <alex_m_nordin@pm.me>
2024-05-31 17:18:49 +0200

    Improve fib actionform

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
 use crate::error_template::{AppError, ErrorTemplate};
+use html::Input;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+// use server_fn::error::ServerFnErrorSerde;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -28,8 +30,10 @@ pub fn App() -> impl IntoView {
             .into_view()
         }>
             <main>
+                <NavBar/>
                 <Routes>
-                    <Route path="" view=Home/>
+                    <Route path="/home" view=Home/>
+                    <Route path="/fib" view=Fib/>
                 </Routes>
             </main>
         </Router>
@@ -67,5 +71,75 @@ fn Home() -> impl IntoView {
                 }}
             </button>
         </main>
+    }
+}
+
+#[component]
+pub fn NavBar() -> impl IntoView {
+    view! {
+        <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto space-x-4 justify-between">
+            <A href="home">"Home"</A>
+            <A href="fib">"Fib"</A>
+        </nav>
+    }
+}
+
+#[server(CalcFib, "/api")]
+pub async fn calc_fib(input: String) -> Result<u128, ServerFnError> {
+    fn fib_memo(n: u128, memo: &mut [u128; 2]) -> u128 {
+        let [a, b] = *memo;
+        let c = a + b;
+
+        if n == 0 {
+            c
+        } else {
+            *memo = [b, c];
+            fib_memo(n - 1, memo)
+        }
+    }
+
+    let target: u128 = input.parse().unwrap_or_default();
+
+    if target < 2 {
+        Ok(1)
+    } else {
+        Ok(fib_memo(target - 2, &mut [0, 1]))
+    }
+}
+
+#[component]
+pub fn Fib() -> impl IntoView {
+    let input_ref = NodeRef::<Input>::new();
+
+    // a server action can be created by using the server function's type name as a generic
+    // the type name defaults to the PascalCased function name
+    let action = create_server_action::<CalcFib>();
+
+    view! {
+        <h3>Using <code>create_action</code></h3>
+        // <p>
+        // "Some server functions are conceptually \"mutations,\", which change something on the server. "
+        // "These often work well as actions."
+        // </p>
+        <input node_ref=input_ref placeholder="Give me an Integer"/>
+        <button on:click=move |_| {
+            let text = input_ref.get().unwrap().value();
+            action.dispatch(text.into());
+        }>
+
+            Submit
+        </button>
+        <p>
+        The result was:
+        {move || if action.value().get().is_none() {
+            "Nothing right now".to_string()
+        } else if action.value().get().is_some() {
+            let eeg = action.value().get().unwrap().unwrap();
+            format!("{}", eeg)
+        } else {
+            format!("{:?}", "shit".to_string())
+        }
+        }
+            </p>
     }
 }
